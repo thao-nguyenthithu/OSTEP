a51b2aa3836bf1fe244c0704410a539c667be67d
ThuTheo <thaonguyenthithu.ws@gmail.com>
2024-01-23 23:40:05 +0700

    add code lab1+2

--- ostep-code/lab2_excel.c
+++ ostep-code/lab2_excel.c
@@ -5,7 +5,7 @@
 #include<sys/wait.h> 
 
 int main(){
-	printf("pid: %d",(int) getpid());
+	printf("pid: %d\n",(int) getpid());
 
 	// tao ban sao cua tien trinh hien tai. 
 	// ham fork() dung de tao tien trinh con
@@ -15,10 +15,17 @@ int main(){
 		printf("fork failed!\n");
 		exit(-1);
 	}else if (sc == 0){
-		printf("Child pid: %d", (int)getpid());
-		execl("/bin/ls","ls","-lt",".",NULL);
-		printf("this shouldn't print out");
-	}else{
+		printf("Child pid: %d\n", (int)getpid());
 		
+		// thay doi tien trinh con bang chuong trinh chi dinh
+		execl("/bin/ls","ls","-lt",".",NULL);
+		
+		// dong nay se khong bao gio duoc thuc hien vi tien trinh con da thay doi chuong trinh cua minh 
+		printf("this shouldn't print out\n");
+	}else{
+		//doi cho tien trinh con ket thuc
+		int wc = wait(NULL);
+		
+		printf("parent of %d sc %d pid %d\n",sc, wc, (int)getpid());
 	}
 }
