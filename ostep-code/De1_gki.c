ef853d9efdb3a72bc89e09b4255ea8c608f63bbe
ThuTheo <thaonguyenthithu.ws@gmail.com>
2024-01-25 12:55:45 +0700

    code de 1 dung ki thuat dong ong

--- ostep-code/De1_gki.c
+++ ostep-code/De1_gki.c
@@ -1,20 +1,54 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int main(char* argv[]){
-
-	int cs = fork();
+int main() {
+	// khoi tao bien luu ten file
+	char filename[100];
 	
-	if(cs < 0){
-		printf("fork failed!\n"); 
-		exit(1);
-	}else if(cs == 0){
-		printf("child process\n");
-		printf("file name: ");
-		scanf("%s",&argv);
-
-		execl("/bin/vim","vi",argv[],NULL);
-
+	//ham pipe dung de tien trinh con doc du lieu tu doi so truyen va ghi vao pp
+	// tien trinh cha doc tu pp va xuat ra.
+	int fp[2];
+	
+	if(pipe(fp)==0) {
+		//Khoi tao chuong trinh con 
+		int pid = fork();
+		
+		if(pid < 0){
+			printf("fork failed\n");
+			return -1;
+		}
+		if(pid == 0){
+			printf("Child\n");
+			printf("Nhap vao ten file khoi tao: ");
+			fflush(stdin);
+			gets(filename);
+			
+			// dong dau doc cua ong trong tt con
+			close(fp[0]);
+			//ghi ten file vao ong de chuyen cho tien trinh cha
+			write(fp[1],filename,strlen(filename));
+			close(fp[1]); // dong dau ghi 
+			
+			execlp("vim","vim", filename, NULL);
+			
+			printf("vim loi!\n");
+				
+		}else{
+			// doi tien trinh con ket thuc 
+			wait(NULL);
+			
+			//doc file tu ong
+			read(fp[0],filename,sizeof(filename));
+			
+			printf("Cha\n");
+			execlp("cat","cat",filename,NULL);
+			
+			printf("cat loi\n");
+			
+			
+		}
 	}
 }
